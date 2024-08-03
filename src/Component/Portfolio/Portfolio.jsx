81c0abb7128ac618fe1888f2eaa713d2b6102b9a
MMohmedMahmoudd <162662856+MMohmedMahmoudd@users.noreply.github.com>
2024-08-03 17:47:03 +0300

    third commit

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -23,7 +23,7 @@ export default function Portfolio() {
 
   return (
     <>
-    <section className="Portfolio pt-14 pb-3  w-full flex justify-center items-center">
+    <section className="Portfolio pt-14 w-full flex justify-center items-center">
     <div className="container pt-14 w-full text-center">
         <div className="data w-3/4 mx-auto">
         <h1 className="text-[#2c3e50] text-5xl max-sm:text-4xl uppercase font-bold pt-16">
@@ -52,7 +52,6 @@ export default function Portfolio() {
               </div>
             ))}
           </div>
-
           <div className="cards flex mb-10 flex-wrap">
             {[img1, img2, img3].map((imgSrc, index) => (
               <div
