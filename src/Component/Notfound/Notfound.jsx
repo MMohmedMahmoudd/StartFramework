f4b945af8fc5a61a17787981341b15d095837ee4
MMohmedMahmoudd <162662856+MMohmedMahmoudd@users.noreply.github.com>
2024-08-03 14:48:21 +0300

    first commit

--- src/Component/Notfound/Notfound.jsx
+++ src/Component/Notfound/Notfound.jsx
@@ -2,7 +2,11 @@
 export default function Notfound() {
   return (
 <>
-<h1>404 page</h1>
+<div className="container py-24 my-24">
+  <div className="parent py-24 my-24">
+    <h1 className="text-slate-900 text-7xl font-bold">404 page</h1>
+  </div>
+</div>
 </>
 )
 }
